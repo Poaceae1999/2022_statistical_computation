893095844552f0d4c9d411d1ec9e753a081c89e0
Poaceae1999 <b07601009@g.ntu.edu.tw>
2022-06-10 17:36:30 +0800

    sace for target(2)

--- 01 code/stat_comp_script.R
+++ 01 code/stat_comp_script.R
@@ -1,6 +1,6 @@
 # gogogo
 # parameters
-
+# hist ----
 b_tau <- 1# i don't know what actually is,for cauchy plus(0,b_tau)
 a <- 1 # for proposal distribution inverse gamma(a,b) 
 b <- 1 # for proposal distribution inverse gamma(a,b)
@@ -14,6 +14,7 @@ dat <- collected_data_from(accept_reject_K_invG,times=50000,a_1=a,b_1=b,a = a,b 
 
 draw(target_dist,dat,b_cauchy=b_tau,a_1=a,b_1=b)
 
+#QQplot----
 percentile <- seq(from = 10^-8,to = 50,by = 0.1)
 pr <- numeric(500L)
 QR <- numeric(20L)
@@ -28,4 +29,42 @@ Q <- quantile(dat, pr[6:25])
 
 qqplot(QR, Q, main="",
            xlab="Target Quantiles", ylab="Sample Quantiles")
+
+# bayes----
+b <- .2 #actual value of beta
+w <- .25 #width of the uniform support set
+m <- 5000 #length of the chain
+burn <- 1000 #burn-in time
+days <- 250
+x <- numeric(m) #the chain
+# generate the observed frequencies of winners
+   i <- sample(1:5, size=days, replace=TRUE,
+                prob=c(1/3, (1-b)/3, (1-2*b)/3, (2*b)/3, b/3))
+win <- tabulate(i)
+print(win)
+
+
+prob <- function(y, win) {
+  # computes (without the constant) the target density
+    if (y < 0 || y >= 0.5)
+       return (0)
+   return(((1/3)^win[1] *
+              ((1-y)/3)^win[2] * ((1-2*y)/3)^win[3] *
+             (2*y)/3)^win[4] * (y/3)^win[5])
+}
+
+u <- runif(m) #for accept/reject step
+ v <- runif(m, -w, w) #proposal distribution
+x[1] <- .25
+for (i in 2:m) {
+   y <- x[i-1] + v[i]
+   if (u[i] <= prob(y, win) / prob(x[i-1], win))
+     x[i] <- y else
+       x[i] <- x[i-1]
+}
+print(win)
+print(round(win/days, 3))
+print(round(c(1, 1-b, 1-2*b, 2*b, b)/3, 3))
+xb <- x[(burn+1):m]
+print(mean(xb))
 
